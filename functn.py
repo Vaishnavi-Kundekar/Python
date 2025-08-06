5246edc78d2157d29d339a409aa2b4de2fc88f45
Vaishnavi Kundekar <vaishnavikundekar3118@gmail.com>
2025-08-06 13:34:56 +0530

    Add files via upload

---
+++ functn.py
@@ -0,0 +1,12 @@
+def show():
+ print("Hello")
+ 
+def accept(rno,name):
+ print(rno,name)
+ 
+def display(fname,sname):
+ print(fname,sname)
+ 
+show()
+accept(11,"vaishnavi")
+display("Gaurav","Vaibhavi")
